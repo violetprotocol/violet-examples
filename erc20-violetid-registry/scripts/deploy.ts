3da9c0974c3da7b8af39d1dd999b708036f6f49f
Gabriel Ferraz <sudoferraz@gmail.com>
2023-11-03 15:59:38 +0200

    Adds variable for VioletID address on OpGoerli

--- erc20-violetid-registry/scripts/deploy.ts
+++ erc20-violetid-registry/scripts/deploy.ts
@@ -3,8 +3,9 @@ import { ethers } from "hardhat";
 async function main() {
 
 
-  // The last argument is the VioletID contract address on optimismGoerli
-  const compliantToken = await ethers.deployContract("CompliantERC20", ["CompliantToken", "CPTK", "0x2a0988b07C538a097Ad8b693369f6e42991591F5"]);
+  // VioletID contract address on optimismGoerli
+  const VIOLETID_REGISTRY_OP_GOERLI = "0x2a0988b07C538a097Ad8b693369f6e42991591F5";
+  const compliantToken = await ethers.deployContract("CompliantERC20", ["CompliantToken", "CPTK", VIOLETID_REGISTRY_OP_GOERLI]);
 
   await compliantToken.waitForDeployment();
 
