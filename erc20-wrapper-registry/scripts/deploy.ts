f394baec100cd33fe14b72e2f9ed647e3297143a
Gabriel Ferraz <sudoferraz@gmail.com>
2023-11-29 11:54:25 +0200

    fix: removes unnecessary comment on deploy script

--- erc20-wrapper-registry/scripts/deploy.ts
+++ erc20-wrapper-registry/scripts/deploy.ts
@@ -3,8 +3,6 @@ import { ethers } from "hardhat";
 async function main() {
 
 
-  // Ethereum Access Token Verifier contract address on optimismGoerli
-  // const ACCESS_TOKEN_VERIFIER = "0x5b66D6bf6395Cfe769B44e5404c7875f95749F80";
   const compliantFactory = await ethers.deployContract("CompliantFactory", []);
 
   await compliantFactory.waitForDeployment();
